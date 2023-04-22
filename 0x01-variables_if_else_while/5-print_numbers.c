c75b1936f45ae6eef727e724f61e10d85766c5aa
Mugambira Justine <justmugambira93@gmail.com>
2023-04-22 04:27:19 -0700

    five

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int i;
-	for (i=0;i<10;i++)
+
+	for (i = 0; i <= 9; i++)
 	{
-		printf("%d",i);
+		printf("%d", i);
 	}
 	putchar('\n');
 	return (0);
