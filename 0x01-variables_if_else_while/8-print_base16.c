01844d01b5d442c2841ce7360f6605598e3af648
Mugambira Justine <justmugambira93@gmail.com>
2023-04-22 03:39:06 -0700

    eight

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int i;
 	char la;
-	for (i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9i'; i++)
 	{
 		putchar(i);
 	}
