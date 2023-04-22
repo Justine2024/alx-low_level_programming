69c9635bf2550578b75f0e3690cc1111dd6c2b06
Mugambira Justine <justmugambira93@gmail.com>
2023-04-22 04:42:08 -0700

    three

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	int i ='a';
-	int j ='A';
-	while (i <='z')
+	int i = 'a';
+	int j = 'A';
+
+	while (i <= 'z')
 	{
-		putchar (i);
+		putchar(i);
 		i++;
 	}
-	while ( j <='Z')
+	while (j <= 'Z')
 	{
-		putchar (j);
+		putchar(j);
 		j++;
 	}
 	putchar ('\n');
