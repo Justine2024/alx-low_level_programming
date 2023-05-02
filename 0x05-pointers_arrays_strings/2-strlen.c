b6f668fd189234ee36c5cb44591425e4b325be2a
Mugambira Justine <justmugambira93@gmail.com>
2023-05-02 12:51:16 -0700

    stringlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - gives the length of a string
+ * @s: string
+ * Return: Length
+ */
+
+int _strlen(char *s)
+{
+	int lingi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
